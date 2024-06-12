ab0e6a140d516be26939f81e01832d88c5fbfd93
Shane Holman <Sholman@tql.com>
2024-06-12 15:16:25 -0400

    Add project files.

---
+++ ConsoleApp1.2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp1._2;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        while (true)
+        {
+
+            Console.Write("Enter an integer (zero to close): ");
+            string answer = Console.ReadLine();
+
+            int aNumber = Convert.ToInt32(answer);
+
+            if (aNumber == 0)
+            {
+                break;
+            }
+
+                if (aNumber % 2 == 0)
+                {
+                    Console.WriteLine($"{aNumber} is even");
+                }
+                else
+                {
+                    Console.WriteLine($"{aNumber} is odd.");
+                }
+            }
+            Console.WriteLine("Done...");
+        }
+    }
+
+
